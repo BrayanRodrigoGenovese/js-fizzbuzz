0b55705e1072301327b7e7cafbb4d7e8cd986e2a
Brayan <bittolo.8bit@gmail.com>
2025-12-17 14:24:27 +0100

    pathway

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Creo un ciclo "for" di 100 numeri
+
+// Controllo: (la "scaletta" di controllo è importante per evitare sovrapposizioni tra "Fizz", "Buzz" e "FizzBuzz")
+//     se "i" è divisibile per 3 e 5 (15), nel log metto FizzBuzz
+//     altrimenti se "i" è divisibile per 3, nel log metto Fizz
+//     altrimenti se "i" è divisibile per 5, nel log metto Buzz
+//     altrimenti nel log metto solo "i"
