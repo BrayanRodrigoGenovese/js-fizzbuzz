40141eb2699260382b340ac2f26ef9888769bf44
Brayan <bittolo.8bit@gmail.com>
2025-12-17 14:24:56 +0100

    code solution

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@
 //     altrimenti se "i" è divisibile per 3, nel log metto Fizz
 //     altrimenti se "i" è divisibile per 5, nel log metto Buzz
 //     altrimenti nel log metto solo "i"
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  } else if (i % 3 === 0) {
+    console.log("Fizz");
+  } else if (i % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(i);
+  }
+}
